45a78bd4b76749fa9d0ac7e789a803df70857773
Alex9220 <93039641+Alex9220@users.noreply.github.com>
2022-04-05 17:22:15 +0300

    Lesson6

---
+++ src/test/java/lesson6/DeleteProductTest.java
@@ -0,0 +1,65 @@
+package lesson6;
+
+import com.github.javafaker.Faker;
+import lesson5.api.ProductService;
+import lesson5.dto.Product;
+import lesson5.utils.RetrofitUtils;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class DeleteProductTest {
+    static ProductService productService;
+    Product product = null;
+    Faker faker = new Faker();
+    int id;
+
+
+    @BeforeAll
+    static void beforeAll() {
+        productService = RetrofitUtils.getRetrofit()
+                .create(ProductService.class);
+    }
+
+    @BeforeEach
+    void setUp() {
+        product = new Product()
+                .withTitle(faker.food().ingredient())
+                .withCategoryTitle("Food")
+                .withPrice((int) (Math.random() * 10000));
+    }
+
+
+
+
+    @Test
+    void deleteProductInFoodCategoryTest() throws IOException {
+        Response<Product> response = productService.createProduct(product)
+                .execute();
+        System.out.println(response.body().getId());
+        id =  response.body().getId();
+
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new
+                SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        lesson6.db.dao.ProductsMapper productsMapper = session.getMapper(lesson6.db.dao.ProductsMapper.class);
+
+        assertThat(productsMapper.deleteByPrimaryKey((long) id), CoreMatchers.is(1));
+    }
+
+
+}
